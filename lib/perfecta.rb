bd39d6e3b6ea4393d06a0386444cd9d00326589f
Gary Rafferty <gary.rafferty@gmail.com>
2013-03-21 22:41:58 +0000

    Add BASE_API_PATH const

--- lib/perfecta.rb
+++ lib/perfecta.rb
@@ -3,7 +3,9 @@ require "perfecta/version"
 module Perfecta
   class Client
 
-    attr_accessor :email, :password
+    BASE_API_PATH = 'https://api.perfectaudience.com/'
+
+    attr_accessor :email, :password, :token
 
     def initialize &block
       yield self if block_given?
