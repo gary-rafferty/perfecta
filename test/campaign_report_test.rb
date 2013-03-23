4a0fc3f509b09f8b2ad4322f7a8b99d19b814a8d
Gary Rafferty <gary.rafferty@gmail.com>
2013-03-23 21:20:53 +0000

    Add unit test for CampaignReport

---
+++ test/campaign_report_test.rb
@@ -0,0 +1,21 @@
+require_relative 'test_helper'
+
+class CampaignReportTest < MiniTest::Unit::TestCase
+
+  describe 'CampaignReport' do
+
+    before do
+      @attrs = {name: 'Report 1', campaign_id: 123}
+      @report = Perfecta::CampaignReport.new @attrs
+    end
+
+    it 'should be created from a hash' do
+      @report.must_be_instance_of Perfecta::CampaignReport
+    end
+
+    it 'should set the values for each property' do
+      @report.name.must_equal 'Report 1'
+      @report.campaign_id.must_equal 123
+    end
+  end
+end
