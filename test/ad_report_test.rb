4bcc2fc8a8ca48fd6b3c321d421592c935a5cc89
Gary Rafferty <gary.rafferty@gmail.com>
2013-03-23 22:13:29 +0000

    Unit tests for AdReport and ConversionReport objects

---
+++ test/ad_report_test.rb
@@ -0,0 +1,21 @@
+require_relative 'test_helper'
+
+class AdReportTest < MiniTest::Unit::TestCase
+
+  describe 'AdReport' do
+
+    before do
+      @attrs = {ad_name: 'Ad 1', ad_id: 123}
+      @report = Perfecta::AdReport.new @attrs
+    end
+
+    it 'should be created from a hash' do
+      @report.must_be_instance_of Perfecta::AdReport
+    end
+
+    it 'should set the values for each property' do
+      @report.ad_name.must_equal 'Ad 1'
+      @report.ad_id.must_equal 123
+    end
+  end
+end
---
+++ test/conversion_report_test.rb
@@ -0,0 +1,21 @@
+require_relative 'test_helper'
+
+class ConversionReportTest < MiniTest::Unit::TestCase
+
+  describe 'ConversionReport' do
+
+    before do
+      @attrs = {conversion_name: 'Conversion 1', conversion_id: 123}
+      @report = Perfecta::ConversionReport.new @attrs
+    end
+
+    it 'should be created from a hash' do
+      @report.must_be_instance_of Perfecta::ConversionReport
+    end
+
+    it 'should set the values for each property' do
+      @report.conversion_name.must_equal 'Conversion 1'
+      @report.conversion_id.must_equal 123
+    end
+  end
+end
