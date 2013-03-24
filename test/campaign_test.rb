3e528d89fe5582614d81b66b096b17761858e418
Gary Rafferty <gary.rafferty@gmail.com>
2013-03-24 17:11:29 +0000

    Add basic unit tests for campaign and site resources

---
+++ test/campaign_test.rb
@@ -0,0 +1,20 @@
+require_relative 'test_helper'
+
+class CampaignTest < MiniTest::Unit::TestCase
+
+  describe 'Campaign' do
+
+    before do
+      @attrs = {name: 'Campaign 1'}
+      @report = Perfecta::Campaign.new @attrs
+    end
+
+    it 'should be created from a hash' do
+      @report.must_be_instance_of Perfecta::Campaign
+    end
+
+    it 'should set the values for each property' do
+      @report.name.must_equal 'Campaign 1'
+    end
+  end
+end
---
+++ test/site_test.rb
@@ -0,0 +1,20 @@
+require_relative 'test_helper'
+
+class SiteTest < MiniTest::Unit::TestCase
+
+  describe 'Site' do
+
+    before do
+      @attrs = {name: 'Site 1'}
+      @report = Perfecta::Site.new @attrs
+    end
+
+    it 'should be created from a hash' do
+      @report.must_be_instance_of Perfecta::Site
+    end
+
+    it 'should set the values for each property' do
+      @report.name.must_equal 'Site 1'
+    end
+  end
+end
